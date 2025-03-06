e004333a434b4e672301a55a29294c0d490906d9
nikhilp-osu <npalamo@okstate.edu>
2025-03-06 15:36:31 -0600

    Initial Commit

---
+++ cricketSCTests/cricketSCTests.swift
@@ -0,0 +1,17 @@
+//
+//  cricketSCTests.swift
+//  cricketSCTests
+//
+//  Created by Palamoni, Nikhil Palamoni on 3/6/25.
+//
+
+import Testing
+@testable import cricketSC
+
+struct cricketSCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
